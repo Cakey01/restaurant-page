f68739dafe35bdfff53a63be49c2ffa5172965b2
Cakey01 <alexanderwarren7@gmail.com>
2025-10-20 12:48:09 -0400

    Create resetDisplay, clickedNav, and displayController functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import { displayHomepage } from './homepage.js';
 import { displayMenu } from './menu.js';
+import { displayAbout } from './about.js';
 import './styles.css';
 import './homepage.css';
 import './menu.css';
+import './about.css';
 
-displayMenu();
+function resetDisplay() {
+    const display = document.getElementById('content');
+    display.innerHTML = '';
+}
+
+function clickedNav(button) {
+    resetDisplay();
+    if (button.id === 'home') {
+        displayHomepage();
+    } else if (button.id === 'menu') {
+        displayMenu();
+    } else if (button.id === 'about') {
+        displayAbout();
+    }
+}
+
+function displayController() {
+    displayHomepage();
+
+    const navs = document.querySelectorAll('button');
+
+    navs.forEach(button => {
+        button.addEventListener('click', () => clickedNav(button));
+    });
+}
+
+displayController();
